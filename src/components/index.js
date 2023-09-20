0f1e7decf96be4c2806861259fb44ffc1dfea0d5
guille <guillermoneculqueo@gmail.com>
2023-09-19 23:02:19 -0300

    cambios

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+import Card from './Card/Card'
+import Header from './Header/Header'
+import CargaDatos from './CargaDatos/CargaDatos'
+import Categorias from './Categorias/Categorias'
+import Footer from './Footer/Footer'
+import Main from './Main/Main'
+
+export default { Card, Header, CargaDatos, Categorias, Footer, Main }
